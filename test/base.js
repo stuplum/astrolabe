6039b7c9711bf0c61ac1369abc1bfd8ae6701cd2
stuplum <stuart.plumbley@bskyb.com>
2013-09-10 09:36:08 +0100

    added helper functions for extending page and module classes

--- test/base.js
+++ test/base.js
@@ -1,5 +1,7 @@
 describe('Base', function() {
 
+    var Base;
+
     beforeEach(function() {
 
         this.mockProtractor = {
@@ -7,7 +9,7 @@ describe('Base', function() {
             getInstance: sinon.stub()
         };
 
-        var Base = Sandbox.require('../lib/astrolabe/base', {
+        Base = Sandbox.require('../lib/astrolabe/base', {
             globals: { protractor: this.mockProtractor }
         });
 
@@ -24,4 +26,48 @@ describe('Base', function() {
 
         this.base.driver.should.be.string('driver instance');
     });
+
+    it('should extend the base class', function() {
+
+        var Extended = Base.extend({
+            newProp: { writable: false, enumerable: false, value: 'new prop value' }
+        });
+
+        expect(new Extended()).to.be.an.instanceof(Base);
+    });
+
+    it('should extend the base class with functions', function() {
+
+        var Extended = Base.extend({}, {
+            newFunc: sinon.stub()
+        });
+
+        var extended = new Extended();
+
+        extended.newFunc('test');
+
+        extended.newFunc.should.have.been.calledWithExactly('test');
+    });
+
+    it('should create an instance extended class', function() {
+
+        var extended = Base.create({
+            newProp: { writable: false, enumerable: false, value: 'new prop value' },
+            newFunc: sinon.stub()
+        });
+
+        expect(extended).to.be.an.instanceof(Base);
+    });
+
+    it('should create an instance with functions', function() {
+
+        var extended = Base.create({}, {
+            newFunc: sinon.stub()
+        });
+
+        extended.newFunc('testInstance');
+
+        extended.newFunc.should.have.been.calledWithExactly('testInstance');
+    });
+
 });
