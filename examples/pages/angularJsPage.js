60fb285030d107b8e1f9e63e2517e337456a4e20
stuplum <stuplum@gmail.com>
2013-09-14 22:27:30 +0100

    updated example page to new api

--- examples/pages/angularJsPage.js
+++ examples/pages/angularJsPage.js
@@ -1,16 +1,10 @@
 var Page = require('../../lib/astrolabe').Page;
 
-var content = {
+module.exports = Page.create({
 
     url: { value: 'http://www.angularjs.org' },
 
     yourName: { get: function() { return this.findElement(this.by.input("yourName")) } },
     greeting: { get: function() { return this.findElement(this.by.binding("{{yourName}}!")); } },
     todo:     { get: function() { return this.findElement(this.by.repeater('todo in todos').row(2)); } }
-};
-
-function AngularJsPage() { Page.call(this); }
-AngularJsPage.prototype = Object.create(Page.prototype, content);
-AngularJsPage.prototype.constructor = AngularJsPage;
-
-module.exports = new AngularJsPage();
+});
